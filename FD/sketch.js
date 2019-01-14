ea4ccdb5da7a2746fa5652eaac88c08e216440d8
wesley-cantarino <wesley.cantarino@engenharia.ufjf.br>
2019-01-14 10:46:43 -0200

    pages

---
+++ FD/sketch.js
@@ -0,0 +1,48 @@
+// Original code by @meiamsome
+
+let categories;
+let category;
+let contraption;
+let dropdown;
+let drawButton;
+var drawingMode = false;
+
+let drawing = [
+  { x: -200, y: -200 },
+  { x: 0, y: -100 },
+  { x: 0, y: 100 },
+  { x: 200, y: 200 }
+];
+
+function gcd(a, b) {
+  return b === 0 ? a : gcd(b, a % b);
+}
+
+function lcm(a, b) {
+  return a * (b / gcd(a, b));
+}
+
+function setup() {
+  let canvas = createCanvas(windowWidth - 20, windowHeight - 20);
+  contraption = new Contraption().load(drawing);
+}
+function windowResized() {
+  resizeCanvas(windowWidth - 20, windowHeight - 20);
+}
+
+let drawingLines = [];
+let drawingInterval = null;
+
+function draw() {
+  background(220);
+  translate(width / 2, height / 2);
+  for (const [xs, ys] of drawingLines) {
+    beginShape();
+    for (let i = 0; i < xs.length; i++) {
+      vertex(xs[i], ys[i]);
+    }
+    endShape();
+  }
+  contraption.step();
+  contraption.render();
+}
